3f7c7bc19bb1f65ae99792738fd0f1c0d89f0bd4
SherreAhlers <sherre.ahlers@gmail.com>
2020-09-19 17:24:05 -0500

    Implement Auth

--- scholarstack/main_app/views.py
+++ scholarstack/main_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 def home(request):
@@ -7,3 +11,16 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
