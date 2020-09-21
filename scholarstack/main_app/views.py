723264cdd62fee3555c3f5e3e1631c6487145d59
SherreAhlers <sherre.ahlers@gmail.com>
2020-09-21 15:28:16 -0500

    Merge Success

--- scholarstack/main_app/views.py
+++ scholarstack/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from main_app.forms import ProfileCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
