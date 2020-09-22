e9252446e65d1709a64596121536014c39dfecea
Cyrena Nielsen <cychey@Cyrenas-MacBook-Air.local>
2020-09-21 23:47:37 -0700

    styling for login and about

--- scholarstack/main_app/urls.py
+++ scholarstack/main_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
