ff02abf01c36eab56acb52f4b1c800ad66e705b2
romain herry <romainherry@MacBook-Pro.local>
2020-09-22 10:28:20 -0700

    add task form on user's profile

--- scholarstack/main_app/urls.py
+++ scholarstack/main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('status_create/', views.StatusCreate.as_view(), name='status_create'),
-
-    # path('accounts/signup/', views.signup, name='signup'),
+    path('profile/<int:profile_id>/', views.profile_detail, name='profile_detail'),
+    path('profile/<int:profile_id>/edit_avatar/', views.edit_avatar, name='edit_avatar'),
+    path('profile/<int:profile_id>/create_task/', views.create_task, name='create_task'),
 ]
--- scholarstack/main_app/views.py
+++ scholarstack/main_app/views.py
@@ -7,7 +7,7 @@ from main_app.forms import ProfileCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Profile
+from .models import Profile, Profile_Avatar
 
 S3_BASE_URL = 'https://s3.us-west-1.amazonaws.com/'
 BUCKET = 'scholarstack'
@@ -17,27 +17,59 @@ BUCKET = 'scholarstack'
 def user_has_profile(request, profile):
     # print('hitting user.profile', len(profile.status))
     if profile.status:
-        print("status is not empty")
+        # print("status is not empty")
         return True
     else:
-        print("status is empty")
+        # print("status is empty")
         return False
 
 
 def home(request):
     if hasattr(request.user, 'profile') and user_has_profile(request, request.user.profile):
     # user_has_profile(request, request.user.profile):
-        print('User is_authenticated and has a profile')
-        return render(request, 'profile_index.html')
+        # print('User is_authenticated and has a profile')
+        profile = Profile.objects.get(id=request.user.profile.id)
+        # avatar = Profile_Avatar.objects.get(profile_id=request.user.profile.id)
+        return redirect('profile_detail', profile_id=profile.id)
     elif request.user.id == None:
-        print('User without profile')
-        return redirect('signup')
+        # print('User without profile')
+        return redirect('login')
     else:
         return redirect('status_create')
 
 
 def about(request):
     return render(request, 'about.html')
+
+def profile_detail(request, profile_id):
+    profile = Profile.objects.get(id=profile_id)
+    task_form = TaskForm()
+    return render(request, 'profile_index.html', {'profile': profile, 'task_form': task_form})
+
+
+def edit_avatar(request, profile_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # We need a unic key / but keep the file extention too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case we get an errot
+    try:
+        s3.upload_fileobj(photo_file, BUCKET, key)
+        url = f"{S3_BASE_URL}{BUCKET}/{key}"
+        # Profile_Avatar.objects.create(url=url, profile_id=profile_id)
+        Profile_Avatar.objects.get(profile_id=profile_id).update(url=url)
+    except:
+        print('An error occured uploading file to S3')
+    return redirect('home')
+
+def create_task(request, profile_id):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        new_task = form.save(commit=False)
+        new_task.profile_id = profile_id
+        new_task.save()
+    return redirect('profile_detail', profile_id=profile_id)
 
 
 def signup(request):
